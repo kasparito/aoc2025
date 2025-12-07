0c058c07987f5f814871294a4fc80f1f1dee0913
kaspar <kaspar@recordedfuture.com>
2025-12-07 13:58:26 +0100

    Day 7 - minimalistic
    Signed-off-by: kaspar <kaspar@recordedfuture.com>

--- src/com/hellden/aoc/year2025/day07/Day7.scala
+++ src/com/hellden/aoc/year2025/day07/Day7.scala
@@ -11,25 +11,12 @@ object Day7 extends Day[BigInt](7):
 
 case class Day7(input: String) extends SolutionFull[BigInt]:
 
-  private val start = input.indexOf('S')
-  private val splitters = input.asLines.map: line =>
-    line.zipWithIndex.collect { case ('^', index) => index }.toSet
+  private val result = input.asLines.foldLeft((beamTimelines = Map(input.indexOf('S') -> 1L), splitCount = 0)):
+    case ((beamTimelines, splitCount), line) =>
+      val splits = line.zipWithIndex.collect { case ('^', index) => index }.toSet
+      val splitBeams = beamTimelines.view.flatMap: (beam, timelines) =>
+        if splits(beam) then Map(beam - 1 -> timelines, beam + 1 -> timelines) else Map(beam -> timelines)
+      (splitBeams.groupMapReduce(_._1)(_._2)(_ + _), splitCount + beamTimelines.keys.count(splits))
 
-  override def part1: BigInt =
-    splitters
-      .foldLeft((beams = Set(start), splitCount = 0)):
-        case ((beams, splitCount), splits) =>
-          val splitBeams = beams.flatMap: beam =>
-            if splits(beam) then Set(beam - 1, beam + 1) else Set(beam)
-          (splitBeams, splitCount + beams.count(splits))
-      .splitCount
-
-  override def part2: BigInt =
-    splitters
-      .foldLeft[Map[Int, BigInt]](Map(start -> 1)):
-        case (beamTimelines, splits) =>
-          val splitBeams = beamTimelines.view.flatMap: (beam, timelines) =>
-            if splits(beam) then Map(beam - 1 -> timelines, beam + 1 -> timelines) else Map(beam -> timelines)
-          splitBeams.groupMapReduce(_._1)(_._2)(_ + _)
-      .values
-      .sum
+  override def part1: BigInt = result.splitCount
+  override def part2: BigInt = result.beamTimelines.values.sum
